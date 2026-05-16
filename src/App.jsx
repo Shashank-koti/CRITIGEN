54955126127bb40fb572f017e89249013e74a80a
shashank <shashankkoti05@gmail.com>
2026-05-16 09:55:27 +0530

    deleted about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import About from './pages/About';
 import CritigenPharma from './pages/CritigenPharma';
 import VisionMission from './pages/VisionMission';
 import Portfolio from './pages/Portfolio';
@@ -19,7 +18,6 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
             <Route path="/about/critigen-pharma" element={<CritigenPharma />} />
             <Route path="/about/vision-mission" element={<VisionMission />} />
             <Route path="/portfolio" element={<Portfolio />} />
